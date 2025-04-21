ac1c3096eb599eb4bb678fda8a23d98659c65421
khang912k4 <khang912k4@gmail.com>
2025-04-22 01:44:09 +0700

    BaiThiGK

---
+++ LamLaiBaiThiGiuaKi/src/test/java/thiGK/ntu64130958/LamLaiBaiThiGiuaKiApplicationTests.java
@@ -0,0 +1,13 @@
+package thiGK.ntu64130958;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LamLaiBaiThiGiuaKiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
